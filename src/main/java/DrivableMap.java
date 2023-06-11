4a06e8c0b444406fe964b11b38726c99f7ff4304
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-06-11 17:24:35 -0400

    finito

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public boolean addDrivable(String id, Drivable item) {
+        if (this.drivable_map.containsKey(id)) {
+            return false;
+        }
+        this.drivable_map.put(id, item);
+        return true;
+
+    }
 
 
 
@@ -37,7 +45,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable item : this.drivable_map.values()) {
+            if (item.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,6 +61,16 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+
+    public ArrayList<Tradable> getTradable() {
+        ArrayList<Tradable> Tradables = new ArrayList<Tradable>();
+        for (Object item : this.drivable_map.values()) {
+            if (item instanceof Tradable) {
+                Tradables.add((Tradable) item);
+            }
+        }
+        return Tradables;
+    }
 
 
 
